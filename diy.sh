7849590271eb37b4288d3da69684f51efcb80573
yangchlin <62425061+yangchlin@users.noreply.github.com>
2020-03-26 13:07:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,13 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
+
+# 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+# 添加Luci主题
+# packages_path=package/openwrt-packages
+# mkdir -p $packages_path
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial $packages_path/luci-theme-atmaterial
+# git clone https://github.com/aboutboy/luci-theme-butongwifi.git
