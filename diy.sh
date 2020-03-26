693d6c5f07d3b9547ef64279a2e3ee663c7c58eb
yangchlin <62425061+yangchlin@users.noreply.github.com>
2020-03-26 16:47:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,7 @@
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
-#修改wifi设置
+# 修改wifi设置
 sed -i 's/OpenWrt/OpenWrt_$(cat /sys/class/ieee80211/${dev}/macaddress|awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 添加Luci主题
@@ -18,4 +18,3 @@ sed -i 's/OpenWrt/OpenWrt_$(cat /sys/class/ieee80211/${dev}/macaddress|awk -F ":
 # mkdir -p $packages_path
 # git clone https://github.com/openwrt-develop/luci-theme-atmaterial $packages_path/luci-theme-atmaterial
 # git clone https://github.com/aboutboy/luci-theme-butongwifi.git
-
