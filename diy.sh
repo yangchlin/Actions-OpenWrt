afaab87e54505c3e36ccdb85e84b41ec46a61fe1
yangchlin <62425061+yangchlin@users.noreply.github.com>
2020-03-26 17:23:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # 修改wifi设置
-sed -i 's/OpenWrt/OpenWrt_$(cat /sys/class/ieee80211/${dev}/macaddress|awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/OpenWrt/OpenWrt_$(cat /sys/class/ieee80211/${dev}/macaddress|awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 添加Luci主题
 # packages_path=package/openwrt-packages
